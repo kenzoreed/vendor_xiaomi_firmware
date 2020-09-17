50fc0b941874a423000a6dec181a47a725e87cfe
allerttt <thesupernikkce@yandex.ru>
2020-09-17 17:35:43 +0300

    firmware: Add makefiles and update README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # vendor_xiaomi_firmware
+This package only for sagit (Xiaomi Mi 6)
---
+++ sagit/firmware.mk
@@ -0,0 +1,17 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/firmware/sagit/abl.elf:install/firmware-update/abl.elf \
+    vendor/xiaomi/firmware/sagit/BTFM.bin:install/firmware-update/BTFM.bin \
+    vendor/xiaomi/firmware/sagit/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/xiaomi/firmware/sagit/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/xiaomi/firmware/sagit/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/xiaomi/firmware/sagit/hyp.mbn:install/firmware-update/hyp.mbn \
+    vendor/xiaomi/firmware/sagit/keymaster.mbn:install/firmware-update/keymaster.mbn \
+    vendor/xiaomi/firmware/sagit/logfs_ufs_8mb.bin:install/firmware-update/logfs_ufs_8mb.bin \
+    vendor/xiaomi/firmware/sagit/logo.img:install/firmware-update/logo.img \
+    vendor/xiaomi/firmware/sagit/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/xiaomi/firmware/sagit/rpm.mbn:install/firmware-update/rpm.mbn \
+    vendor/xiaomi/firmware/sagit/splash.img:install/firmware-update/splash.img \
+    vendor/xiaomi/firmware/sagit/storsec.mbn:install/firmware-update/storsec.mbn \
+    vendor/xiaomi/firmware/sagit/tz.mbn:install/firmware-update/tz.mbn \
+    vendor/xiaomi/firmware/sagit/xbl.elf:install/firmware-update/xbl.elf \
